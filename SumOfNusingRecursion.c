9cbb919c49e8679f93c7036959735b01d859d741
rashikoranne <58441854+rashikoranne@users.noreply.github.com>
2021-07-17 12:43:43 +0530

    Add files via upload

---
+++ SumOfNusingRecursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* sum of n natural numbers by recursion */
+
+int sum(int n)
+{
+    if(n == 0)
+    {
+        return 0;
+
+    }
+    else{
+        return sum(n-1) + n;
+    }
+}
+
+
+int main()
+{
+    int r;
+    r = sum(5);
+    printf("%d", r);
+    return 0;
+}
